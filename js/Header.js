6311b0f585fbb96a2d75bad490e53fbeff3727d9
mMDsergiu <catanoi.sergiu@gmail.com>
2023-02-26 02:18:00 +0200

    02:17

--- js/Header.js
+++ js/Header.js
@@ -4,51 +4,53 @@ const mainUrl = isHome ? '' : '../Index.html';
 const isHome1 = document.getElementById('header').getAttribute('data-page') === 'home';
 const mainUrl1 = isHome ? '' : '.';
 
-const isHome2 = document.getElementById('header').getAttribute('data-page') === 'home';
-const mainUrl2 = isHome ? '' : '.';
+
 
 
 document.getElementById('header').innerHTML = `  
-<input type="checkbox" aria-label="Toggle menu"/>
-<span></span>
-<span></span>
-<span></span>
-
-
-<a href="https://www.instagram.com/neonlux_shop/" target="_blank" class="meniu-logo">
-  <img src="${mainUrl2}./img/Pozele Home/Logo/Psite-01.png" alt="NeonLux">
-</a>
-
-
-<div class="meniu">
-  <ul>
- 
-    <li>
-    <a href="${mainUrl}#AnchorNoi">Despre Noi</a>
-    </li>
-    <li>
-      <a href="${mainUrl}#AnchorGlarie">Galerie</a>
-    </li>
-    <li>
-      <a href="${mainUrl}#AnchorProduse">Produse</a>
-    </li>
-    <li>
-      <a href="${mainUrl}#AnchorContact">Contact</a>
-    </li>
+<nav class="navbar">
+<div class="content">
+  <div class="logo">
+    <a href="https://www.instagram.com/neonlux_shop/"  target="_blank">NeonLux</a>
+  </div>
+  <ul class="menu-list">
+    <div class="icon cancel-btn">
+      <i class="fas fa-times"></i>
+    </div>
+    <li><a href="${mainUrl}#AnchorNoi">Despre Noi</a></li>
+    <li><a href="${mainUrl}#AnchorGlarie">Galerie</a></li>
+    <li><a href="${mainUrl}#AnchorProduse">Produse</a></li>
+    <li><a href="${mainUrl}#AnchorContact">Contact</a></li>
 
   </ul>
-  <ul>
-    <li>
-      <a href="${mainUrl1}./pages/Register.html" >
-        Creare Cont
-      </a>
-    </li>
-    <li>
-      <a href="${mainUrl2}./pages/Login.html" >
-        Logare
-      </a>
-    </li>
-
+  <div class="icon menu-btn">
+    <i class="fas fa-bars"></i>
+  </div>
+  <ul class="log">
+  <li><a href="${mainUrl1}./pages/Register.html">Creare Cont</a></li>
+  <li><a href="${mainUrl1}./pages/Login.html">Logare</a></li>
   </ul>
-</div>`;
+</div>
+</nav>
+<div class="banner"></div>
 
+`;
+
+
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+}
+cancelBtn.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+window.onscroll = ()=>{
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
