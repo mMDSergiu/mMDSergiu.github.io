6b995b81de362753188189af9e66697a97d35af3
mMDsergiu <catanoi.sergiu@gmail.com>
2023-04-26 13:13:24 +0300

    treb

--- js/LightMode.js
+++
@@ -1,9 +0,0 @@
-var icon = document.getElementById("icon");
-icon.onclick = function(){
-  document.body.classList.toggle("white-theme");
-  if(document.body.classList.contains("white-theme")){
-    icon.src = "./img/Special/BulbOff.png";
-  }else{
-    icon.src = "./img/Special/BulbOn.png"
-  }
-}
